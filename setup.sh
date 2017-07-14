8ad7295b8b03cf9685da34da8d05bfa183e119f8
Mystique <13oct08@quantumfoam.uni.cc>
2017-07-14 23:24:46 +0530

    helper bash install script
    Signed-off-by: Mystique <13oct08@quantumfoam.uni.cc>

---
+++ setup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -ex
+
+### Install Runtime libraries
+curl -O https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm \
+&& yum -y install epel-release-latest-7.noarch.rpm
+yum -y install python-pip git
+pip install --upgrade pip
+pip install virtualenv
+
+# Clone our app from git
+cd /var
+git clone https://github.com/miztiik/flask-news-app.git
+cd /var/flask-news-app
+
+### Create a Virtual Environment for our App
+virtualenv /var/flask-news-app
+source /var/flask-news-app/bin/activate
+
+### Install the App dependencies
+pip install -r requirements.txt
+
+# Start `gunicorn`
+# Start the `gunicorn` and bind it port `8000` and listen on all interfaces
+gunicorn --bind 0.0.0.0:8000 wsgi:APP &
+
+# Deactivate
+deactivate
+
