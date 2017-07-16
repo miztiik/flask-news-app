013c8c9b9183314059970404bb8528b10d86a948
Mystique <13oct08@quantumfoam.uni.cc>
2017-07-16 22:46:15 +0530

    added missing \ and correction application name
    Signed-off-by: Mystique <13oct08@quantumfoam.uni.cc>

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,8 +2,8 @@
 
 # Start Gunicorn processes
 echo "Starting Gunicorn"
-exec gunicorn appsrc.wsgi:application \
+exec gunicorn appsrc.wsgi:APP \
     --name flask-news-app \
     --bind 0.0.0.0:8000 \
-    --pythonpath '/var/flask-news-app'
+    --pythonpath '/var/flask-news-app' \
     "$@"
