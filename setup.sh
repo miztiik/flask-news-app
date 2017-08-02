7ad747ead3d5e30656506d9e3e4ccb1481ca951d
Mystique <13oct08@quantumfoam.uni.cc>
2017-08-02 19:17:30 +0530

    Changed "APP" to "application" to comply with AWS Elastic Beanstalk requirements
    Signed-off-by: Mystique <13oct08@quantumfoam.uni.cc>

--- setup.sh
+++ setup.sh
@@ -23,7 +23,7 @@ pip install gunicorn
 
 # Start `gunicorn`
 # Start the `gunicorn` and bind it port `8000` and listen on all interfaces
-gunicorn --bind 0.0.0.0:8000 wsgi:APP &
+gunicorn --bind 0.0.0.0:8000 wsgi:application &
 
 # Deactivate
 deactivate
