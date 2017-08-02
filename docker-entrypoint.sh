379feadac48897cd7b9a6ed72eafb06481cc1ca6
Mystique <13oct08@quantumfoam.uni.cc>
2017-08-02 19:17:03 +0530

    configuring the docker script to use the "application" term instead of "APP"
    Signed-off-by: Mystique <13oct08@quantumfoam.uni.cc>

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 # Start Gunicorn processes
 echo "Starting Gunicorn"
-exec gunicorn appsrc.wsgi:APP \
+exec gunicorn appsrc.wsgi:application \
     --name flask-news-app \
     --bind 0.0.0.0:8000 \
     --pythonpath '/var/flask-news-app' \
