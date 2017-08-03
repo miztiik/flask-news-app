7fb83a956cf8ceb53cf9fa959ed645b89b36fbc5
Mystique <13oct08@quantumfoam.uni.cc>
2017-08-04 00:09:02 +0530

    Good to run in Elastic Beanstalk & Docker & locally :)
    Signed-off-by: Mystique <13oct08@quantumfoam.uni.cc>

--- application.py
+++ application.py
@@ -205,4 +205,4 @@ def newsSpider():
                             )
 
 if __name__ == "__main__":
-    application.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)))
+    application.run(host=os.getenv('IP', '0.0.0.0'))
